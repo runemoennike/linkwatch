ed52a590c4c91fd936c4e96675af3520e091cec7
rm <rm@kirkeweb.dk>
2012-03-19 20:07:56 +0100

    Accepted hosts can be removed

--- templates/host_list.tpl.php
+++ templates/host_list.tpl.php
@@ -1,6 +1,6 @@
 <ul>
 <?php foreach($HOSTS as $host) :
 	if(!is_array($host)) continue; ?>
-	<li><?php echo $host['pattern']?></li>
+	<li><?php echo $host['pattern']?> (<?php echo interface_link('Remove', array('page' => 'hostmgmt', 'action' => 'delete', 'host' => $host['pattern'])); ?>)</li>
 <?php endforeach; ?>
 </ul>
---
+++ templates/host_remove_submit_status.tpl.php
@@ -0,0 +1,5 @@
+<p>
+	Removed <i><?php echo $host ?></i> as an accepted host for links.
+</p>
+
+<?php echo interface_link('Return to Overview', array('page' => 'overview')); ?>
