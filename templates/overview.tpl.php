e13728347c640889b5a162f65a8d3d61310b1c4d
rm <rm@kirkeweb.dk>
2012-03-19 20:02:17 +0100

    Added a link to start a crawl

--- templates/overview.tpl.php
+++ templates/overview.tpl.php
@@ -1,11 +1,12 @@
 <h2>Stats</h2>
 <?php echo templates_exec('stats_simple'); ?>
 
-<h2>Watching</h2>
+<h2>Sites being Watched</h2>
 <?php echo interface_link('Add Site', array('page' => 'sitemgmt', 'action' => 'add')); ?>
 <?php echo templates_exec('site_list', array()) ?>
 
 <h2>Last crawl output</h2>
+<?php echo interface_link('Crawl Now', 'crawl.php') ?>
 <?php echo templates_exec('log_output'); ?>
 
 <h2>Configuration</h2>
